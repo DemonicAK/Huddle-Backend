e5e58d5e378ea272eb7d3e83f0b16b7afe836cce
arijitkar <demonicak07@gmail.com>
2023-09-30 22:01:55 +0530

    many changes

--- models/User.js
+++ models/User.js
@@ -1,92 +1,73 @@
-const mongoose = require("mongoose");
-const { isEmail } = require("validator");
-const bcrypt = require("bcryptjs");
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Please enter a name"],
-    },
-    email: {
-      type: String,
-      required: [true, "Please enter an email"],
-      unique: true,
-      lowercase: true,
-      validate: [isEmail, "Please enter a valid email"],
-      index: true,
-    },
-    password: {
-      type: String,
-      required: [true, "Please enter a password"],
-      minLength: [4, "Minimum password length is 6 characters"],
-    },
-    picture: {
-      type: String,
-    },
-    newMessage: {
-      type: Object,
-      default: {},
-    },
-    status: {
-      type: String,
-      default: "online",
-    },
+const mongoose = require('mongoose');
+const {isEmail} = require('validator');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Can't be blank"]
   },
-  { minimize: false }
-);
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: [true, "Can't be blank"],
+    index: true,
+    validate: [isEmail, "invalid email"]
+  },
+  password: {
+    type: String,
+    required: [true, "Can't be blank"]
+  },
+  picture: {
+    type: String,
+  },
+  newMessages: {
+    type: Object,
+    default: {}
+  },
+  status: {
+    type: String,
+    default: 'online'
+  }
+}, {minimize: false});
 
-
-
-// fire a function before doc saved to db
-userSchema.pre('save',  function (next) {
+UserSchema.pre('save', function(next){
   const user = this;
-  if (!user.isModified('password')) return next();
+  if(!user.isModified('password')) return next();
 
-   bcrypt.genSalt(10, function (err, salt) {
-    if (err) return next(err);
- // hash the password along with our new salt
-    bcrypt.hash(user.password, salt, function (err, hash) {
-      if (err) return next(err);
-    // override the cleartext password with the hashed one
-      user.password = hash;
+  bcrypt.genSalt(10, function(err, salt){
+    if(err) return next(err);
+
+    bcrypt.hash(user.password, salt, function(err, hash){
+      if(err) return next(err);
+
+      user.password = hash
       next();
-    });
-  });
-});
+    })
+
+  })
+
+})
 
 
-
-
-
-// when return user object to user, delete password and newMessage
-userSchema.methods.toJSON = function () {
+UserSchema.methods.toJSON = function(){
   const user = this;
   const userObject = user.toObject();
-
   delete userObject.password;
-  // delete userObject.newMessage;
-
   return userObject;
-};
+}
 
-// static method to login user
-userSchema.statics.findByCredentials = async function (email, password) {
-  const user = await this.findOne({ email });
-  if (!user) {
-    throw new Error("Invalid email");
-  }
+UserSchema.statics.findByCredentials = async function(email, password) {
+  const user = await User.findOne({email});
+  if(!user) throw new Error('invalid email or password');
 
-  const isPasswordMatch = await bcrypt.compare(password, user.password);
+  const isMatch = await bcrypt.compare(password, user.password);
+  if(!isMatch) throw new Error('invalid email or password')
+  return user
+}
 
 
-  if (!isPasswordMatch) {
-    console.log(password, user.password);
-    throw new Error("Invalid password");
-  }
+const User = mongoose.model('User', UserSchema);
 
-  return user;
-};
-
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
+module.exports = User
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,43 +1,38 @@
-const express = require("express");
-const router = express.Router();
-const User = require("../models/User");
-const dotenv = require("dotenv");
-dotenv.config();
+const router = require('express').Router();
+const User = require('../models/User');
 
-router.post("/createuser", async (req, res) => {
-try {
-    const { name, email, password,picture } = req.body;
-    console.log(req.body);
-    const user = await User.create({ name, email, password,picture });
-    res.status(201).json({ user });
-    
-} catch (error) {
-    
-let msg;
-if(error.code === 11000){
-    msg = "Email already exists";
-
-}else{
-    msg=error.message;
-}
-console.log(error);
-res.status(400).json({error:msg});
-}
-
-
-});
-
-router.post("/login", async (req, res) => {
+// creating user
+router.post('/', async(req, res)=> {
   try {
-    const { email, password } = req.body;
+    const {name, email, password, picture} = req.body;
     console.log(req.body);
-    const user = await User.findByCredentials(email, password);
-    user.status = "online";
-    await user.save();
-    res.status(200).json({ user });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
+    const user = await User.create({name, email, password, picture});
+    res.status(201).json(user);
+  } catch (e) {
+    let msg;
+    if(e.code == 11000){
+      msg = "User already exists"
+    } else {
+      msg = e.message;
+    }
+    console.log(e);
+    res.status(400).json(msg)
   }
-});
+})
 
-module.exports = router;
+// login user
+
+router.post('/login', async(req, res)=> {
+  try {
+    const {email, password} = req.body;
+    const user = await User.findByCredentials(email, password);
+    user.status = 'online';
+    await user.save();
+    res.status(200).json(user);
+  } catch (e) {
+      res.status(400).json(e.message)
+  }
+})
+
+
+module.exports = router
