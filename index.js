7459c5c601062279dd9526b9a258ba757d9e91a5
ArijitKar <demonicak07@gmail.com>
2025-01-30 15:57:55 +0530

    corrected the url

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const cors = require("cors");
 app.use(
   cors({
-    origin: "https://huddle-backend-nm6l.onrender.com",
+    origin: "https://huddle-client.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
