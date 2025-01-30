a850cedc51bc2b584a5c4d7f2677cfc9b9bad698
ArijitKar <demonicak07@gmail.com>
2025-01-30 15:20:23 +0530

    some updates

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ const io = require("socket.io")(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
-    transports: ["websocket"],
+    credentials: true,
+    // transports: ["websocket"],
   },
   allowEIO3: true,
 });
